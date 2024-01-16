2a8eb223dd30a3d817c598c33f8e64bbeb582dca
Piyush singh <piyushsingh@Piyushs-MacBook-Pro.local>
2024-01-16 15:29:13 +0530

    added animation

--- src/components/home/animation.tsx
+++ src/components/home/animation.tsx
@@ -15,9 +15,8 @@ export default function Animation() {
   };
 
   const getCardClassName = (index: number): string => {
-
     let bgColorClass;
-    
+
     switch (index) {
       case 0:
         bgColorClass = "bg-[#272339]";
@@ -26,95 +25,93 @@ export default function Animation() {
         bgColorClass = "bg-[#273177]";
         break;
       case 2:
-        bgColorClass = "bg-[#273177]";
+        bgColorClass = "bg-[#00061d]";
         break;
       default:
         bgColorClass = ""; // default background color class if needed
     }
-    return `card ${hovered === index ? "hovered" : ""} ${bgColorClass}`;
+    return `card p-4 ${hovered === index ? "hovered" : ""} ${bgColorClass}`;
   };
-
 
   const getTextClassName = (index: number): string => {
     return `text-details ${hovered === index ? "hovered" : ""}`;
   };
   return (
-    <div className="relative w-full h-[600px] border border-solid border-black">
-      <div className="flex justify-between mx-auto max-w-[1444px] h-[600px]">
+    <div className="relative w-full  animation-container ">
+      <div className="flex justify-between">
         {/* Data Visualization */}
-        <div
+        <button
           className={getCardClassName(0)}
           onMouseEnter={() => handleMouseEnter(0)}
           onMouseLeave={handleMouseLeave}
-  
         >
-          <div className="w-[280px]  h-[280px] mt-20 flex justify-start items-center">
+          {" "}
+          <div className="text-white text-3xl font-semibold text-left mt-4">
+            Data Visualization
+          </div>
+          <div className="  h-[50%]  flex justify-start items-center">
             <Image
               height={200}
               width={200}
-              className="w-full h-full object-cover"
               alt="Data Visualization"
               src="https://c.animaapp.com/MXeDvc3F/img/-9d6d9e55-709a-4703-b411-51bd0c201705-1-1.svg"
             />
-          </div>
-          <div className="text-white text-3xl font-semibold text-left mt-4">
-            Data Visualization
           </div>
           <p className={getTextClassName(0)}>
             We turn data into your strategic ally, unlocking insights that drive
             growth and informed decision-making for maximum impact
           </p>
-        </div>
+        </button>
 
         {/* Big Data */}
-        <div
+        <button
           className={getCardClassName(1)}
           onMouseEnter={() => handleMouseEnter(1)}
           onMouseLeave={handleMouseLeave}
         >
-          <div className="w-[314px] h-[255px] mt-20  flex justify-start items-center">
+          {" "}
+          <div className="text-white text-3xl font-semibold text-left mt-4">
+            Big Data
+          </div>
+          <div className="  h-[50%] flex justify-start items-center">
             <Image
               height={200}
               width={200}
-              className="w-full h-full object-cover"
               alt="Big Data"
               src="https://c.animaapp.com/MXeDvc3F/img/etl-1.svg"
             />
-          </div>
-          <div className="text-white text-3xl font-semibold text-left mt-4">
-            Big Data
           </div>
           <p className={getTextClassName(1)}>
             From efficient warehousing to powerful analytics, we turn big data
             challenges into opportunities, driving smarter decisions for your
             business success
           </p>
-        </div>
+        </button>
 
         {/* Machine Learning */}
-        <div
+        <button
           className={getCardClassName(2)}
           onMouseEnter={() => handleMouseEnter(2)}
           onMouseLeave={handleMouseLeave}
         >
-          <div className="w-[377px] h-[283px] mt-12  flex justify-start items-center">
+          =
+          <div className="text-white text-3xl font-semibold text-left mt-4">
+            Machine Learning
+          </div>
+          <div className=" h-[50%] flex justify-start items-center">
             <Image
               height={200}
               width={200}
-              className="w-full h-full object-cover"
               alt="Machine Learning"
               src="https://c.animaapp.com/MXeDvc3F/img/ai-brain-learning-scaled-e1689603131824-1.svg"
             />
-          </div>
-          <div className="text-white text-3xl font-semibold text-left mt-4">
-            Machine Learning
           </div>
           <p className={getTextClassName(2)}>
             From end-to-end model management to powerful inference, we bring AI
             solutions that propel your success through data-driven insights and
             innovation
           </p>
-        </div>
+        </button>
       </div>
     </div>
   );
