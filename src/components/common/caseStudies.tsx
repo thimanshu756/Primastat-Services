f1fc54ad6d79d6ad07907a77f9635e19784c7fa2
piyushsingh0992 <piyushsingh0992@gmail.com>
2024-02-04 04:03:23 +0530

    changes for the case studeies

--- src/components/common/caseStudies.tsx
+++ src/components/common/caseStudies.tsx
@@ -13,17 +13,12 @@ interface ClientProps {
 }
 
 const Client: React.FC<ClientProps> = ({ caseStudiesList }) => {
-  // Function to determine the grid column class
-  const getGridClass = () => {
-    const listLength = caseStudiesList.length;
-    if (listLength === 1) return "grid-cols-1";
-    return "grid-cols-2";
-  };
+
 
   return (
     <div id="CaseStudies" className="bg-[#00388B] mx-auto p-4 flex flex-col items-center justify-center w-[100vw] pb-24">
       <p className="text-white text-3xl py-12 text-bold">Our Work</p>
-      <div className={`grid grid-cols-1 md:${getGridClass()} gap-4 md:w-[70vw]`}>
+      <div className={`grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-24 md:w-[70vw]`}>
         {caseStudiesList.map(({ image, message, route }, index) => (
           <Link key={index} href={route} passHref>
             <div className="block p-6 flex flex-col items-center cursor-pointer">
