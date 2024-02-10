96fece9d50620070e9e3792ab3fb2027952ff3ec
Shivansh Kaushik <shivansh.kaushik@nexxo.com>
2024-02-10 16:39:14 +0530

    reviews corrected

--- src/data/clientTestimonies.ts
+++ src/data/clientTestimonies.ts
@@ -8,23 +8,24 @@ export interface clientDetails {
   
 
 export const clientList:Array<clientDetails> = [
+
+
   {
     imgLink: "/public/client-testimony/review-1.svg",
-    testimony:
-      "Primastat's data analytics transformed our business, delivering actionable insights and driving exponential growth.",
-
-    name: "Tushar Aggarwal",
-    designation: "CEO",
-    location: "TUAG",
-  },
-
-  {
-    imgLink: "/public/client-testimony/review-2.svg",
     testimony:
       "Primastat's work is brilliant and their attention to detail, especially considering the complexities of our requirement was excellent.",
 
     name: "Alwyn Veliyeth",
     designation: "CTO",
     location: "Rezcomm",
+  },
+  {
+    imgLink: "/public/client-testimony/review-2.svg",
+    testimony:
+      "Primastat's data analytics transformed our business, delivering actionable insights and driving exponential growth.",
+
+    name: "Tushar Aggarwal",
+    designation: "CEO",
+    location: "TUAG",
   },
 ];
